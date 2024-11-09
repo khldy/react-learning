982342bad9364461a8db4d1c5b32923edd36301a
khldy <20212022103@std.nisantasi.edu.tr>
2024-11-09 20:39:36 +0300

    dynamic styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import { EXAMPLES } from "./data-with-examples.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components"); //Components is the default selected topic
+  const [selectedTopic, setSelectedTopic] = useState(); //Components is the default selected topic
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -33,12 +33,30 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
               Components
             </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
           {!selectedTopic ? (
             <p>Please select a topic</p>
